8d447a7536bd4b923de583f312bca7eb1cc031a8
Xigbar68 <jakes349123@gmail.com>
2024-04-16 17:43:16 -0700

    Created constructor for JeopardyQAndA. See example csv.

--- src/JeopardyQAndA.java
+++ src/JeopardyQAndA.java
@@ -1,6 +1,7 @@
-import java.util.ArrayList;
-import java.util.Hashtable;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
+import java.io.*;
 
 /**
  * Lead Author(s):
@@ -12,8 +13,11 @@ import java.util.*;
  * <<add additional contributors (mentors, tutors, friends) here, with contact information>>
  * 
  * References:
+ * Creating an ArrayList with multiple object types in Java. (2021, October 22). GeeksforGeeks. https://www.geeksforgeeks.org/creating-an-arraylist-with-multiple-object-types-in-java/
+ * 
+ * Hashtable put() method in Java. (2018, June 28). GeeksforGeeks. https://www.geeksforgeeks.org/hashtable-put-method-in-java/
  *  
- * Version/date: Version 1, 4/11/2024
+ * Version/date: Version 1, 4/16/2024
  * 
  * Responsibilities of class:
  * Non-GUI file
@@ -29,29 +33,98 @@ public class JeopardyQAndA
 	// and an element of an array list containing question, answer, hint, and category in that order
 	
 	// Use IO scanner section content in order to read from a CSV for data ease
-	private Hashtable<Integer, ArrayList<String>> questionsAndAnswers;
-	private int questionCount = 0;
+	private Hashtable<Integer, ArrayList<Object>> questionsAndAnswers;
+	private int categoryNumber;
 	
-	public JeopardyQAndA()
+	public JeopardyQAndA(String fileName)
 	{
+		// setup scanner
+		Scanner scan = null;
+		int count = 0;
+		int questionCount = 0;
+		String categoryIndicator = "C-";
+		this.questionsAndAnswers = new Hashtable<Integer, ArrayList<Object>>();
+		
+		try 
+		{
+			File myFile = new File(fileName);	
+			scan = new Scanner(myFile); //Create a Scanner object using the File object
+			ArrayList<Object> categoryQAndA = null;
+			ArrayList<String> questionAnswerHintArray = null; // Used for nested ArrayLists
+			
+			// Obtain content from the file as long as there is content to be obtained 
+			while(scan.hasNext()) 
+			{ 
+				 String data = scan.next(); // scan next, variable saved as 'data'
+				 
+				 // If file reads with the indicator in scan, then
+				 if (data.startsWith(categoryIndicator))
+				 {
+					 // If categoryQAndA has already been through this, add it to the list
+					 if (categoryQAndA != null)
+					 {
+						count++; // increase count for keeping track of categories
+						questionsAndAnswers.put(count, categoryQAndA);
+					 }
+					 categoryQAndA = new ArrayList<Object>(); // Reset categoryQAndA
+					 categoryQAndA.add(data); // Adds new category
+					 
+					 questionCount = 0;
+					 questionAnswerHintArray = new ArrayList<String>(); // Reset questionAnswerHintArray
+				 }
+				 else
+				 {
+					questionCount++;
+					questionAnswerHintArray.add(data);
+					
+					if (questionCount == 3)
+					{
+						questionCount = 0;
+						categoryQAndA.add(questionAnswerHintArray); // Adds question, answer, hint 
+						questionAnswerHintArray = new ArrayList<String>();
+					}
+					
+				 }
+			}
+			
+			count++;
+			questionsAndAnswers.put(count, categoryQAndA); // Adds last category
+			categoryNumber = count;
+		}
+		
+		catch (FileNotFoundException e) 
+		{  
+		    // This block of code runs if the file is not found e.printStackTrace(); 
+			e.printStackTrace(); 
+		} 
+		
+		finally 
+		{
+		   if(scan!=null)
+		    {
+		       scan.close();
+		    } 
+		}
+		
 		
 	}
 	
-	public String getElement(int questionNumber)
+	// hw popup
+	public ArrayList<Object> getCategoryQuestionsAndAnswers(int categoryNumber)
 	{
-		// TODO
-		return null;
-	}
-	
-	public void addQuestion(String[] questionAnswerHint)
-	{
-		// TODO
+		return questionsAndAnswers.get(categoryNumber);
 	}
 	
 	public ArrayList<String> getCategories()
 	{
-		// TODO
+		// TODOs
 		return null;
 	}
+	
+	public Hashtable<Integer, ArrayList<Object>> getQAndA()
+	{
+		return questionsAndAnswers;
+	}
+	
 }
 	
