35c56051ed5321c77ccfa3faa79e6dd1e95737d4
Xigbar68 <jakes349123@gmail.com>
2024-05-04 19:36:09 -0700

    Interface for style methods added, might use or remove later

---
+++ src/StyleMethods.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Font;
+
+/**
+ * Lead Author(s):
+ * @author Jake Salzer
+ * @author Hernany Torres
+ * <<add additional lead authors here, with a full first and last name>>
+ * 
+ * Other contributors:
+ * <<add additional contributors (mentors, tutors, friends) here, with contact information>>
+ * 
+ * References:
+ *  
+ * Version/date: Version 1, 5/4/2024
+ * 
+ * Responsibilities of class:
+ * GUI file
+ */
+
+public interface StyleMethods
+{
+	// Sets the colors of the object
+	void setColors();
+	
+	// Sets the fonts of the object
+	void setFonts();
+}
